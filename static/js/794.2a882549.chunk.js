d9974512071e8e47439bae84960501356852fb09
Sashaegypt <107362302+Sashaegypt@users.noreply.github.com>
2023-02-26 15:35:07 +0000

    Deploying to gh-pages from @ Sashaegypt/goit-react-hw-05-movies@a2eedda21213817d0ae0049c3c50907211cd82a8 🚀

---
+++ static/js/794.2a882549.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page"})]})}}}]);
+//# sourceMappingURL=794.2a882549.chunk.js.map
